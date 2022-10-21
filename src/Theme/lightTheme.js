422861ab15e63f45af70d4f31570b1d845af7706
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-10-21 03:15:32 +0200

    feat: Add theme colors
    Add collected theme colors

--- src/Theme/lightTheme.js
+++ src/Theme/lightTheme.js
@@ -8,6 +8,32 @@ const lightTheme = {
   id: "light",
   // The canvas color is the background color of the website
   canvas: "#DAE0E6",
+  // The colors of the text on the website
+  color: {
+    // The color of the text on the website
+    primary: "#1c1c1c",
+    // The blue color of the website
+    secondary: "#0079d3",
+    // The color of the muted text on the website
+    muted: "#7c7c7c",
+    // The color of the links on the website
+    link: "#0079d3",
+    // The orange color of the website
+    orange: "#d93a00",
+  },
+  // The colors of the backgrounds in the website
+  background: {
+    // The primary color of the background on the website
+    primary: "#fff",
+    // The color of the muted background on the website
+    muted: "#D3D3D3",
+    // The pink color of the website
+    pink: "#fb133a",
+  },
+  // The colors of the lines in the website
+  lineColor: {
+    primary: "#edeff1",
+  },
 };
 
 export default lightTheme;
