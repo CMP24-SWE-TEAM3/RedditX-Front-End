7b313a7e46d5224eb0bca25f3c945388d658e753
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-11-12 22:04:06 +0200

    fix: add search routing in search input

--- src/Components/InputForm/InputForm.jsx
+++ src/Components/InputForm/InputForm.jsx
@@ -1,7 +1,8 @@
 // imports
-import React, {useState} from 'react';
-import {StyledSearcherInput} from "./InputForm.styled";
+import React, { useState } from "react";
+import { StyledSearcherInput } from "./InputForm.styled";
 import SearchDropDown from "../SearchDropDown/SearchDropDown";
+import { useNavigate } from "react-router-dom/dist";
 
 /**
  * Component that displays   a search field with search results
@@ -10,21 +11,33 @@ import SearchDropDown from "../SearchDropDown/SearchDropDown";
  */
 
 const InputForm = () => {
-    /**
-     * state of search field that show trending posts
-     */
-    const [show, setsShow] = useState(false);
-    const handleDropdown = () => {
-        setsShow(!show);
+  /**
+   * state of search field that show trending posts
+   */
+  const [show, setsShow] = useState(false);
+  const handleDropdown = () => {
+    setsShow(!show);
+  };
+  const navigate = useNavigate();
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      navigate("/search/posts");
     }
-    return (
-        <>
-            <StyledSearcherInput id={'search-input'} onClick={handleDropdown} type="text" placeholder={'Search Reddit'}/>
-            {/*<SearchDropDown show={show}/>*/}
-        </>
-    );
+  };
+  return (
+    <>
+      <StyledSearcherInput
+        id={"search-input"}
+        onKeyDown={handleKeyDown}
+        onClick={handleDropdown}
+        type="text"
+        placeholder={"Search Reddit"}
+      />
+      {/*<SearchDropDown show={show}/>*/}
+    </>
+  );
 };
 
 export default InputForm;
-
-
