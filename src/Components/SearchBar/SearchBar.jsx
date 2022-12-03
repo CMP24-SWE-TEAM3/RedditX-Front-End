f7bf5fe8272946bfccb4e31f1b2abd5422e7ccf2
ZiadSheriif <zsherif308@gmail.com>
2022-12-04 00:24:46 +0200

    fix: fix search bar

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,100 +1,26 @@
 // imports
 import React from "react";
-import { SearchItem } from "./SearchBar.styled";
-import { ReactSearchAutocomplete } from "react-search-autocomplete";
-import { ItemContainer } from "./SearchBar.styled";
-import { useAuth } from "Features/Authentication/Contexts/Authentication";
-import useFetchFunction from "Hooks/useFetchFunction";
-import { useContext, useEffect } from "react";
-import SearchContext from "Features/Search/Contexts/SearchWordContext/Search-context";
-import fetchCommunities from "Features/Search/Services/fetchCommunities";
-import fetchPeople from "Features/Search/Services/fetchPeople";
-import { useNavigate } from "react-router-dom/dist";
+import { StyledSearchButton, StyledSearchIcon } from "./SearchBar.styled";
+import InputForm from "Components/InputForm/InputForm";
+import SearchDropDown from "Components/SearchDropDown/SearchDropDown";
+import { useState } from "react";
+
 /**
  * Component that displays the search results for a given search term.
  *
  * @returns {React.Component}
  */
 const SearchBar = () => {
-  const auth = useAuth();
-
-  // navigation to search page
-  const navigate = useNavigate();
-  const ctx = useContext(SearchContext);
-
-  // fetch communities
-  let [CommunityList, errorCommunity, loadingCommunity, fetchCommunity] =
-    useFetchFunction();
-  // console.log("communities", CommunityList);
-
-  // fetch People
-  let [PeopleList, errorPeople, loadingPeople, FB] = useFetchFunction();
-  // console.log("People", PeopleList);
-
-  const searchWord = "text";
-  ctx.wordHandler(searchWord);
-  useEffect(() => {
-    if (ctx.isLoggedIn) {
-      fetchCommunities(fetchCommunity, auth, ctx.word);
-      fetchPeople(FB, auth, ctx.word);
-    }
-  }, [ctx.word]);
-
-  const formatResult = (item) => {
-    return <ItemContainer>{item.name}</ItemContainer>;
-  };
-
-  // dummy data
-  const items = [
-    {
-      id: 0,
-      name: "C",
-    },
-    {
-      id: 1,
-      name: "JavaScript",
-    },
-    {
-      id: 2,
-      name: "Basic",
-    },
-    {
-      id: 3,
-      name: "PHP",
-    },
-    {
-      id: 4,
-      name: "Java",
-    },
-    {
-      id: 5,
-      name: "React",
-    },
-    {
-      id: 6,
-      name: "Angular",
-    },
-  ];
-
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter") {
-      // Cancel the default action, if needed
-      event.preventDefault();
-      ctx.wordHandler(event.target.value);
-      navigate("/search/posts");
-    }
-  };
-
+  // state that toggle the search dropdown
+  const [show, setsShow] = useState(false);
   return (
-    <SearchItem onKeyDown={handleKeyDown}>
-      <ReactSearchAutocomplete
-        placeholder="Search Reddit"
-        styling={SearchItem}
-        items={items}
-        autoFocus
-        formatResult={formatResult}
-      />
-    </SearchItem>
+    <>
+      <StyledSearchButton>
+        <StyledSearchIcon />
+        <InputForm setsShow={setsShow} show={show} />
+        <SearchDropDown show={show} />
+      </StyledSearchButton>
+    </>
   );
 };
 
--- src/Components/SearchBar/SearchBar.styled.js
+++ src/Components/SearchBar/SearchBar.styled.js
@@ -1,45 +1,22 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import {CiSearch} from "react-icons/ci";
 
-export const ItemContainer = styled.span`
-  display: block;
-  text-align: left;
-`;
-export const SearchItem = styled.div`
-  z-index: 1;
-
-  //.bTvxAB{
-  //  position: initial;
-  //}
-  width: 500px;
+export const StyledSearchButton = styled.div`
+  display: flex;
+  border-radius: 20px;
+  height: 40px;
+  //width: 35%;
+  background: ${({theme}) => theme.background.post_background};
+  margin: auto;
+  //width:100%;
   @media (min-width: 1286px) {
     min-width: 516px;
   }
-  @media (max-width:992px){
-      width: 100% !important;
-  }
-  color: ${({ theme }) => theme.color.primary};
 
-  .wrapper {
-    background-color: ${({ theme }) => theme.background.post_background};
-    min-height: 40px !important;
-    box-shadow: none;
-    border-radius: 20px;
-    font-size: 14px;
-    
-    :active,
-    :hover,
-    :focus {
-      border-color: ${({ theme }) => theme.color.secondary};
-      outline: none;
-    }
-
-    ::placeholder {
-      font-size: 14px;
-      color: ${({ theme }) => theme.lineColor.secondary};
-    }
-
-    :focus-within {
-      box-shadow: 0 0 !important;
-    }
-  }
-`;
+`
+export const StyledSearchIcon = styled(CiSearch)`
+  color: ${({theme}) => theme.color.muted};
+  position: absolute;
+  margin-top: 10px;
+  min-width: 50px;
+`
