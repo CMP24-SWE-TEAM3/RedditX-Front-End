9a4d633f57b0c91aaf6437a2937de75ea08f8e3b
khaled-farahat <84389471+khaled-farahat@users.noreply.github.com>
2022-12-05 16:10:21 +0200

    feat : Add Email , Feed and privacy pages

--- src/Features/settings/Layouts/PrivacyPage/PrivacyPage.jsx
+++ src/Features/settings/Layouts/PrivacyPage/PrivacyPage.jsx
@@ -1,5 +1,27 @@
+import BlockPeople from "Features/settings/Components/BlockPeople/BlockPeople";
+import CompContainer from "Features/settings/Components/CompContainer/CompContainer";
+import { Header } from "Features/settings/Layouts/EmailsPage/EmailsPage.styled";
+import { Text } from "./PrivacyPage.styled";
+// import { Link } from "react-router-dom";
+
 const PrivacyPage = () => {
-  return <h1>PrivacyPage</h1>;
+  return (
+    <>
+      <Header>Safety & Privacy</Header>
+      <Text>
+        Manage how we use data to personalize your Reddit experience, and
+        control how other redditors interact with you. To learn more, visit our
+        <span>
+          <a href="https://www.reddithelp.com/hc/en-us/categories/360003246511" target="blank">
+            Privacy & Security FAQs .
+          </a>
+        </span>
+      </Text>
+      <CompContainer headerTitle="Safety">
+        <BlockPeople />
+      </CompContainer>
+    </>
+  );
 };
 
 export default PrivacyPage;
