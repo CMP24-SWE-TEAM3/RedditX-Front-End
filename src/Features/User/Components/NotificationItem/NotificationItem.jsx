94f454dbb21bfcef8a21e04612a1ef7667e9ce07
AbdelrahmanAshrafMohamedelsayed <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2022-12-14 07:01:07 +0200

    feat: Add PopUp

--- src/Features/User/Components/NotificationItem/NotificationItem.jsx
+++ src/Features/User/Components/NotificationItem/NotificationItem.jsx
@@ -1,51 +1,70 @@
 import {
-    ComponentDiv,
-    LeftDiv,
-    RightDiv,
-    HeaderDiv,
-    ComponentHeader,
-    ComponentContent,
-    HeaderSpan,
-    SeparateorSpan,
-    DateSpan,
-    DotsButton,
-    ProfileImage,
+  ComponentDiv,
+  LeftDiv,
+  RightDiv,
+  HeaderDiv,
+  ComponentHeader,
+  ComponentContent,
+  HeaderSpan,
+  SeparateorSpan,
+  DateSpan,
+  DotsButton,
+  ProfileImage,
+  StyledPopup,
+  HideButton,
 } from "./NotificationItem.styled";
 import ParseDateFromNow from "Features/User/Utils/ParseDateFromNow";
 import { useState } from "react";
+import React from "react";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+import { Link } from "react-router-dom";
+const NotificationItem = ({ header, content, date }) => {
+  const [showPrompt, setShowPrompt] = useState(false);
+  const handleClick = () => {
+    setShowPrompt((prev) => !prev);
+  };
+  return (
+    <ComponentDiv
+      to={"#"}
+      onClick={(e) => {
+        e.preventDefault();
+        // handleClick();
+      }}
+    >
+      <LeftDiv>
+        <ProfileImage src="https://styles.redditmedia.com/t5_l3aq2/styles/profileIcon_nlatl3ukwgm41.jpg?width=64&height=64&frame=1&crop=64:64,smart&v=enabled&s=b67e54b1762b178464eddc8007cdb6e268741581" />
+      </LeftDiv>
 
-const NotificationItem = ({header, content, date}) => {
-    const [showPrompt, setShowPrompt] = useState(false);
-    const handleClick = ()=> {
-        setShowPrompt((prev)=>!prev);
-    }
-    return (
-        <ComponentDiv>
-            <LeftDiv>
-                <ProfileImage src="https://styles.redditmedia.com/t5_l3aq2/styles/profileIcon_nlatl3ukwgm41.jpg?width=64&height=64&frame=1&crop=64:64,smart&v=enabled&s=b67e54b1762b178464eddc8007cdb6e268741581"/>
-            </LeftDiv>
-
-            <RightDiv>
-                <HeaderDiv>
-                    <HeaderSpan>
-                        <ComponentHeader>
-                            {header}
-                        </ComponentHeader>
-                        <SeparateorSpan> . </SeparateorSpan>
-                        <DateSpan>{ParseDateFromNow(date).toDateString()}</DateSpan>
-                    </HeaderSpan>
-                    <DotsButton onClick={handleClick}>…</DotsButton>
-                    {/* <div className={showPrompt?"dropdown-active":"dropdown"}>
+      <RightDiv>
+        <HeaderDiv>
+          <HeaderSpan>
+            <ComponentHeader>{header}</ComponentHeader>
+            <SeparateorSpan> . </SeparateorSpan>
+            <DateSpan>{ParseDateFromNow(date).toDateString()}</DateSpan>
+          </HeaderSpan>
+          {/* <DotsButton onClick={handleClick}>…</DotsButton> */}
+          {/* <div className={showPrompt?"dropdown-active":"dropdown"}>
                         <a>click here for a scam</a>
                     </div> */}
-                </HeaderDiv>
-                <ComponentContent>
-                    {content}
-                </ComponentContent>
-            </RightDiv>
+          {/* <Popup trigger={<button>Trigger</button>} position="bottom 100%">
+            {(close) => (
+              <div>
+                Content here
+                <Link className="close" onClick={close}>
+                  &times;
+                </Link>
+              </div>
+            )}
+          </Popup> */}
+          <Popup trigger={<DotsButton> ...</DotsButton>} position="bottom 100%">
+            <HideButton className="Hide">Hide this notification</HideButton>
+          </Popup>
+        </HeaderDiv>
+        <ComponentContent>{content}</ComponentContent>
+      </RightDiv>
+    </ComponentDiv>
+  );
+};
 
-        </ComponentDiv>
-    );
- };
-  
-  export default NotificationItem;
+export default NotificationItem;
--- src/Features/User/Components/NotificationItem/NotificationItem.styled.js
+++ src/Features/User/Components/NotificationItem/NotificationItem.styled.js
@@ -1,35 +1,38 @@
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
-import {HiOutlineReply} from "react-icons/hi";
-export const ComponentDiv = styled(NavLink)`
-    display: flex;
-    padding: 16px;
-    text-decoration: none;
+import { HiOutlineReply } from "react-icons/hi";
+import Popup from "reactjs-popup";
+
+export const ComponentDiv = styled.div`
+  display: flex;
+  padding: 16px;
+  text-decoration: none;
+  & .popup-content {
+    background-color: #000 !important;
+  }
 `;
 
-
 export const LeftDiv = styled.span`
-    padding-right: 8px;
-    position: relative;
+  padding-right: 8px;
+  position: relative;
 `;
 
 export const ProfileImage = styled.img`
-    border-radius: 50%;
-    height: 32px;
-    width: 32px;
-    background-color: ${({ theme }) => theme.borderColor.primary};
-    display: block;
-    object-fit: cover;
+  border-radius: 50%;
+  height: 32px;
+  width: 32px;
+  background-color: ${({ theme }) => theme.borderColor.primary};
+  display: block;
+  object-fit: cover;
 `;
 
 export const HeaderDiv = styled.span`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 4px;
 
-    
-    /* .dropdown{
+  /* .dropdown{
         display: none;
         position: relative;
         right: 10px;
@@ -41,110 +44,128 @@ export const HeaderDiv = styled.span`
         right: 10px;
         top: 5px;
     } */
+  position: relative;
 `;
 
-export const HeaderSpan = styled.span`
-`;
+export const HeaderSpan = styled.span``;
 
 export const SeparateorSpan = styled.span`
-    font-size: 12px;
-    font-weight: 400;
-    line-height: 16px;
-    color: ${({ theme }) => theme.color.muted};
-
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 16px;
+  color: ${({ theme }) => theme.color.muted};
 `;
 
 export const DateSpan = styled.span`
-    font-family: Noto Sans,Arial,sans-serif;
-    font-size: 14px;
-    font-weight: 400;
-    color: ${({ theme }) => theme.color.muted};
-    line-height: 19px;
+  font-family: Noto Sans, Arial, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.muted};
+  line-height: 19px;
 `;
 
 export const ComponentHeader = styled.span`
-    font-size: 16px;
-    line-height: 20px;
-    color: ${({ theme }) => theme.color.primary};
-    overflow-wrap: anywhere;
-    font-family: Noto Sans,Arial,sans-serif;
-    font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${({ theme }) => theme.color.primary};
+  overflow-wrap: anywhere;
+  font-family: Noto Sans, Arial, sans-serif;
+  font-weight: 400;
 `;
 
 export const DotsButton = styled.div`
-    align-items: center;
-    border-radius: 4px;
-    display: flex;
-    justify-content: center;
-    width: 32px;
-    margin-left: 10px;
-    flex: 0 0 32px;
+  align-items: center;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  width: 32px;
+  margin-left: 10px;
+  flex: 0 0 32px;
 
-    /* Default */
-    background: transparent;
-    border: none;
-    color: inherit;
-    cursor: pointer;
-    padding: initial;
+  /* Default */
+  background: transparent;
+  border: none;
+  color: inherit;
+  cursor: pointer;
+  padding: initial;
 
-    &:hover {
-        background-color: ${({ theme }) => theme.button.muted};
-    }
-
+  &:hover {
+    background-color: ${({ theme }) => theme.button.muted};
+  }
 `;
 
 export const ComponentContent = styled.span`
-    font-size: 14px;
-    color: ${({ theme }) => theme.color.muted};
-    word-break: break-word;
-    line-height: 18px;
-    max-height: 54px;
+  font-size: 14px;
+  color: ${({ theme }) => theme.color.muted};
+  word-break: break-word;
+  line-height: 18px;
+  max-height: 54px;
 
-    font-family: Noto Sans,Arial,sans-serif;
-    font-weight: 400;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: normal;
+  font-family: Noto Sans, Arial, sans-serif;
+  font-weight: 400;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: normal;
 `;
 
 export const RightDiv = styled.span`
-    flex: 1 0;
+  flex: 1 0;
 `;
 
 export const ButtonDiv = styled.a`
-    position: relative;
-    background-color: ${({ theme }) => theme.button.muted};
-    color: ${({ theme }) => theme.color.secondary};
-    fill: ${({ theme }) => theme.color.secondary};
+  position: relative;
+  background-color: ${({ theme }) => theme.button.muted};
+  color: ${({ theme }) => theme.color.secondary};
+  fill: ${({ theme }) => theme.color.secondary};
 
-    &::before{ 
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        border-radius: 9999px;
-        opacity: 0;
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 9999px;
+    opacity: 0;
 
-        &:hover {
-            border-color: #24a0ed1a;
-            background: #24a0ed1a;
-        }
-
-        &:focus {
-            outline: none;
-        }
+    &:hover {
+      border-color: #24a0ed1a;
+      background: #24a0ed1a;
     }
-`;
 
-export const ReplyButton = styled.button`
-    
+    &:focus {
+      outline: none;
+    }
+  }
 `;
 
 export const Arrow = styled(HiOutlineReply)`
-    font-size: 17px;
-    height: 17px;
-    line-height: 15px;
-    padding-right: 8px;
+  font-size: 17px;
+  height: 17px;
+  line-height: 15px;
+  padding-right: 8px;
+`;
+// export const StyledPopup = styled(Popup)`
+//   .popup-content,
+//   [role="tooltip"].popup-content {
+//     background-color: #000 !important;
+//     position: relative !important;
+//   }
+// `;
+export const HideButton = styled.div`
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 18px;
+  color: ${({ theme }) => theme.color.primary};
+  display: block;
+  padding: 8px;
+  text-align: left;
+  width: 100%;
+  background: transparent;
+  border: none !important;
+  cursor: pointer;
+  &:hover {
+    border: none !important;
+    color: ${({ theme }) => theme.color.muted};
+  }
 `;
