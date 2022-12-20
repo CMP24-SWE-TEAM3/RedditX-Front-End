9826b6d614c08f4eaa6b759cbf4ad13aca82b2cf
ZiadSheriif <zsherif308@gmail.com>
2022-12-20 23:35:01 +0200

    feat: push notifications

--- src/Components/NotificationButton/NotificationButton.jsx
+++ src/Components/NotificationButton/NotificationButton.jsx
@@ -4,6 +4,7 @@ import { GlobalButtonStyled } from "Components/GlobalButton/GlobalButton.styled"
 import React, { useState, useRef, useEffect } from "react";
 import Overlay from "react-bootstrap/Overlay";
 import Popover from "react-bootstrap/Popover";
+import { BASE_URL } from "API/axios";
 import {
   Container,
   Content,
@@ -23,6 +24,8 @@ import {
   Info,
   SpanContainer,
   Footer,
+  AlarmIcon,
+  NotyIconStyle,
 } from "./NotificationButton.styled";
 import { BsThreeDots, BsClipboardCheck } from "react-icons/bs";
 import { FiSettings } from "react-icons/fi";
@@ -34,6 +37,14 @@ import pushNotifications from "Services/pushNotifications";
 import useFetchFunction from "Hooks/useFetchFunction";
 import { useAuth } from "Features/Authentication/Contexts/Authentication";
 import { useNavigate } from "react-router-dom";
+import getUser from "Features/User/Services/getUser";
+import getCommunityInfo from "Features/Post/Services/getCommunityInfo";
+import useLocalStorage from "Hooks/useLocalStorage";
+import {
+  onForegroundMessage,
+  requestPermission,
+} from "PushNotification/messaging_init_in_sw";
+import { FormLabel } from "Features/Post/Components/DraftLinkForm/DraftLinkForm.styled";
 
 /**
  * Component that displays notifications about changes in the state of the application
@@ -50,6 +61,88 @@ const NotificationButton = () => {
    * state to track if mouse clicks outside notification's button or not
    */
   const [target, setTarget] = useState(null);
+
+  // push notification
+  const [notifications, setNotifications] = useLocalStorage(
+    "notificationId",
+    JSON.stringify([])
+  );
+  const [data, setData] = useLocalStorage("dataId", JSON.stringify([]));
+  const [title, setTitle] = useLocalStorage("titleId", "null");
+  const [body, setBody] = useLocalStorage("boydId", "null");
+  const [type, setType] = useLocalStorage("typeId", "null");
+  const [showAlert, setShowAlert] = useState("alart", "null");
+
+  const [followerId, setFollowerId] = useLocalStorage("userId", "null");
+  const [postId, setPostId] = useLocalStorage("postId", "null");
+  const [commentId, setCommentId] = useLocalStorage("commentId", "null");
+  const [replyId, setReplyId] = useLocalStorage("replyId", "null");
+
+  // useFetchFunction
+  const [userInfo, errorUserInfo, loading, fetchUser] = useFetchFunction();
+  const [post, errorPostInfo, isloading, fetchPost] = useFetchFunction();
+
+  useEffect(() => {
+    onForegroundMessage()
+      .then((payload) => {
+        console.log("Received foreground message: ", payload);
+        const {
+          notification: { title, body },
+        } = payload;
+        const { data } = payload;
+        setNotifications(JSON.stringify([...notifications, { title, body }]));
+        setData(JSON.stringify(data));
+        setType(data.type);
+        setTitle(title);
+        setBody(body);
+        if (data.type === "1") setFollowerId(data.followerID);
+        else if (data.type === "2") setPostId(data.postID);
+        else if (data.type === "3" || type === "4") {
+          setCommentId(data.commentID);
+          setPostId(data.postID);
+        } else if (data.type === "5") {
+          setCommentId(data.commentID);
+          setReplyId(data.replyID);
+        }
+      })
+      .catch((err) =>
+        console.log(
+          "An error occured while retrieving foreground message. ",
+          err
+        )
+      );
+  });
+
+  // get user info
+  useEffect(() => {
+    if (type === "1") {
+      getUser(fetchUser, followerId, auth);
+      setShowAlert(true);
+    }
+  }, [followerId]);
+
+  // get post
+  useEffect(() => {
+    if (type === "2") {
+      getCommunityInfo(fetchPost, postId, auth);
+      setShowAlert(true);
+    }
+  }, [postId]);
+  // get comment
+  useEffect(() => {
+    if (type === "4") {
+      getCommunityInfo(fetchPost, commentId, auth);
+      setShowAlert(true);
+    }
+  }, [commentId]);
+
+  // get reply
+  useEffect(() => {
+    if (type === "5") {
+      getCommunityInfo(fetchPost, replyId, auth);
+      setShowAlert(true);
+    }
+  }, [replyId]);
 
   // ref the event listener
   const ref = useRef(null);
@@ -91,7 +184,21 @@ const NotificationButton = () => {
    */
   const handleClick = (event) => {
     setTarget(event.target);
+    setShowAlert(false);
     setShow(!show);
+  };
+  const handleRoutes = () => {
+    if (type === "1") {
+      navigate(`/user/${followerId}`);
+    } else if (type === "2") {
+      navigate(`/post-preview/${postId}`);
+    } else if (type === "3") {
+      navigate(`/post-preview/${postId}/${commentId}`);
+    } else if (type === "4") {
+      navigate(`/post-preview/${postId}`);
+    } else if (type === "5") {
+      navigate(`/post-preview/${postId}/${commentId}`);
+    }
   };
   useOutsideAlerter(wrapperRef);
 
@@ -104,7 +211,12 @@ const NotificationButton = () => {
   useEffect(() => {
     pushNotifications(fetchData, auth);
   }, []);
-
+  console.log("Dataa=>>", data);
+  console.log("TYPE==>", type);
+  console.log("Post", postId);
+  console.log("USer==>", followerId);
+  console.log("comment", postId);
+  console.log("reply", postId);
   const navigate = useNavigate();
   return (
     <Container ref={ref}>
@@ -119,7 +231,10 @@ const NotificationButton = () => {
           id="notification-box"
           onClick={handleClick}
         >
-          <IoMdNotificationsOutline />
+          <NotyIconStyle>
+            {showAlert && <AlarmIcon>1</AlarmIcon>}
+            <IoMdNotificationsOutline size={22} />
+          </NotyIconStyle>
         </GlobalButtonStyled>
       </OverlayTrigger>
       <Overlay
@@ -150,6 +265,37 @@ const NotificationButton = () => {
           </Popover.Header>
           <Popover.Body data-testid={"notificationListId"}>
             <PopContainer />
+            {data.type && (
+              <Content onClick={handleRoutes}>
+                <ContentSymbol>
+                  <Link>
+                    {userInfo && userInfo.about && userInfo.user && (
+                      <ImageContainer>
+                        <img
+                          src={`${BASE_URL}/users/files/${userInfo.about.user.avatar}`}
+                          alt={"community-name"}
+                        />
+                      </ImageContainer>
+                    )}
+                    <SpanContainer>
+                      <Child>
+                        <SubChild>
+                          <Info>{title}</Info>
+                          {/* <Dot>.</Dot>
+                          <Time>{}h</Time> */}
+                        </SubChild>
+                        <DotBtn>
+                          <DotBtn>
+                            <BsThreeDots />
+                          </DotBtn>
+                        </DotBtn>
+                      </Child>
+                      <InfoChild>{body}</InfoChild>
+                    </SpanContainer>
+                  </Link>
+                </ContentSymbol>
+              </Content>
+            )}
             <Content>
               {notificationList &&
                 notificationList.length !== 0 &&
