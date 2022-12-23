b2a025df3aef4b05ff9f7c8a64d3a8f0312f7856
AbdelrahmanAshrafMohamedelsayed <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2022-12-23 22:42:40 +0200

    feat:add test

--- src/Features/Search/Services/__test__/joinCommunity.test.js
+++ src/Features/Search/Services/__test__/joinCommunity.test.js
@@ -1,115 +1,59 @@
-// import axios from "API/axios";
-// import joinCommunity from "../joinCommunity";
+import joinCommunity from "Features/Subreddit/Services/joinCommunity";
+import axios from "API/axios";
 
-// const mockFetchData = jest.fn(function (config) {
-//   return config;
-// });
-// const mockAuth = {
-//   isLoggedIn: () => false,
-//   getToken: () => false,
-// };
-// const , { id: "t5_Amazing" } = {
-//   isLoggedIn: () => true,
-//   getToken: () => false,
-// };
-// const mockAuthWithToken = {
-//   isLoggedIn: () => false,
-//   getToken: () => "token",
-// };
-// const mockAuthWithTokenAndLogin = {
-//   getToken: () => "token",
-//   isLoggedIn: () => true,
-// };
-// describe("joinCommunity", () => {
-//   it("Should not submit if auth is missing", () => {
-//     joinCommunity(mockFetchData);
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if token is missing && not logged in", () => {
-//     joinCommunity(mockFetchData, mockAuth, "data");
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if not logged in", () => {
-//     joinCommunity(mockFetchData, mockAuthWithLogin, "data");
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if not logged in and token is available", () => {
-//     joinCommunity(mockFetchData, mockAuthWithToken, "data");
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should submit if logged in and token is available", () => {
-//     joinCommunity(mockFetchData, mockAuthWithTokenAndLogin, "data");
-//     expect(mockFetchData).toBeCalledWith({
-//       axiosInstance: axios,
-//       method: "post",
-//       url: "/api/user/subscribe/",
-//       requestConfig: {
-//         data: "data",
-//         headers: {
-//           "Content-Language": "en-US",
-//           authorization: `Bearer token`,
-//         },
-//       },
-//     });
-//   });
-// });
-//
-it("this is a test", () => {});
-//
-// import axios from "API/axios";
-// import joinCommunity from "../joinCommunity";
-
-// const mockFetchData = jest.fn(function (config) {
-//   return config;
-// });
-// const mockAuth = {
-//   isLoggedIn: () => false,
-//   getToken: () => false,
-// };
-// const mockAuthWithLogin = {
-//   isLoggedIn: () => true,
-//   getToken: () => false,
-// };
-// const mockAuthWithToken = {
-//   isLoggedIn: () => false,
-//   getToken: () => "token",
-// };
-// const mockAuthWithTokenAndLogin = {
-//   getToken: () => "token",
-//   isLoggedIn: () => true,
-// };
-// describe("joinCommunity", () => {
-//   it("Should not submit if auth is missing", () => {
-//     joinCommunity(mockFetchData);
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if token is missing && not logged in", () => {
-//     joinCommunity(mockFetchData, mockAuth, { id: "t5_Amazing" });
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if not logged in", () => {
-//     joinCommunity(mockFetchData, mockAuthWithLogin, { id: "t5_Amazing" });
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should not submit if not logged in and token is available", () => {
-//     joinCommunity(mockFetchData, mockAuthWithToken, { id: "t5_Amazing" });
-//     expect(mockFetchData).not.toHaveBeenCalled();
-//   });
-//   it("Should submit if logged in and token is available", () => {
-//     joinCommunity(mockFetchData, mockAuthWithTokenAndLogin, {
-//       id: "t5_Amazing",
-//     });
-//     expect(mockFetchData).toBeCalledWith({
-//       axiosInstance: axios,
-//       method: "POST",
-//       url: `/api/user/subscribe`,
-//       requestConfig: {
-//         data: { id: "t5_Amazing" },
-//         headers: {
-//           "Content-Language": "en-US",
-//           Authorization: `Bearer token`,
-//         },
-//       },
-//     });
-//   });
-// });
+const mockFetchData = jest.fn(function (config) {
+  return config;
+});
+const mockAuth = {
+  isLoggedIn: () => false,
+  getToken: () => false,
+};
+const mockAuthWithLogin = {
+  isLoggedIn: () => true,
+  getToken: () => false,
+};
+const mockAuthWithToken = {
+  isLoggedIn: () => false,
+  getToken: () => "token",
+};
+const mockAuthWithTokenAndLogin = {
+  getToken: () => "token",
+  isLoggedIn: () => true,
+};
+describe("joinCommunity", () => {
+  it("Should not submit if auth is missing", () => {
+    joinCommunity(mockFetchData);
+    expect(mockFetchData).not.toHaveBeenCalled();
+  });
+  it("Should not submit if token is missing && not logged in", () => {
+    joinCommunity(mockFetchData, { id: "t5_Amazing" }, mockAuth);
+    expect(mockFetchData).not.toHaveBeenCalled();
+  });
+  it("Should not submit if not logged in", () => {
+    joinCommunity(mockFetchData, { id: "t5_Amazing" }, mockAuthWithLogin);
+    expect(mockFetchData).not.toHaveBeenCalled();
+  });
+  it("Should not submit if not logged in and token is available", () => {
+    joinCommunity(mockFetchData, { id: "t5_Amazing" }, mockAuthWithToken);
+    expect(mockFetchData).not.toHaveBeenCalled();
+  });
+  it("Should submit if logged in and token is available", () => {
+    joinCommunity(
+      mockFetchData,
+      { id: "t5_Amazing" },
+      mockAuthWithTokenAndLogin
+    );
+    expect(mockFetchData).toBeCalledWith({
+      axiosInstance: axios,
+      method: "POST",
+      url: `/api/user/subscribe`,
+      requestConfig: {
+        data: { id: "t5_Amazing" },
+        headers: {
+          "Content-Language": "en-US",
+          Authorization: `Bearer token`,
+        },
+      },
+    });
+  });
+});
