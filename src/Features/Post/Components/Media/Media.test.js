84885ba7df5b737bb28b56399f06dab3ff334539
Abd-ELrahmanHamza <68310502+Abd-ELrahmanHamza@users.noreply.github.com>
2022-12-23 23:05:12 +0200

    test: test components

---
+++ src/Features/Post/Components/Media/Media.test.js
@@ -0,0 +1,25 @@
+import { shallow } from "enzyme";
+
+import Media from "./Media";
+
+const contentState = {
+  getEntity: function () {
+    return {
+      getType: function () {},
+      getData: function () {
+        return {
+          src: "test",
+        };
+      },
+    };
+  },
+};
+
+const block = {
+  getEntityAt: function () {},
+};
+describe("Button Component", () => {
+  it("this is a test for Button Component", () => {
+    shallow(<Media contentState={contentState} block={block} />);
+  });
+});
